898c45eab138a04b751521e53461344adcf7f9f3
bh-fst02 <bh.fst02@gmail.com>
2020-06-23 11:34:20 +0200

    create layout

---
+++ frontend/src/components/NavigationComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="dark">
+    <b-navbar-brand to="/">Leaderboard</b-navbar-brand>
+    <b-navbar-toggle target="navbar-toggle-collapse">
+      <template v-slot:default="{ expanded }">
+        <b-icon icon="collection-fill" v-if="expanded"></b-icon>
+        <b-icon icon="collection" v-else ></b-icon>
+      </template>
+    </b-navbar-toggle>
+    <b-collapse id="navbar-toggle-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item to="/">TopScores</b-nav-item>
+        <b-nav-item to="/registration">Sign Up</b-nav-item>
+        <b-nav-item to="/login">Sign In</b-nav-item>
+        <b-nav-item to="/about">Profile</b-nav-item>
+        <b-nav-item>Sign Out</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'NavigationComponent',
+};
+</script>
+
+<style scoped>
+button:focus {
+  outline: 0;
+}
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,29 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import TopScores from '../views/TopScores.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'TopScores',
+    component: TopScores,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/registration',
+    name: 'Registration',
+    component: () => import('../views/Registration.vue'),
+  },
+  {
+    path: '/signin',
+    name: 'SignIn',
+    component: () => import('../views/SignIn.vue'),
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: () => import('../views/Profile.vue'),
   },
 ];
 
